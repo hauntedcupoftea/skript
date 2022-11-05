9b18893f9f5bd77d133fa7ab37080b7f20d020ad
hauntedcupoftea <andydchauhan@gmail.com>
2022-11-06 04:59:09 +0530

    finished basic atttribute analysis, finished analyser.py

--- skriptAnalyser.py
+++ skriptAnalyser.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
-from collections import Counter
-import matplotlib.pyplot as plt
 import re
 import tensorflow as tf
 import spacy
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.compat.v1.Session(config=config)
+spacy.prefer_gpu()
+nlp = spacy.load('en_core_web_lg')
 
 def clean(text):
     sample = np.array(re.split(r'(\. )|(\n)', text))
@@ -19,6 +19,28 @@ def clean(text):
     for i in words:
         for x in i:
             x.replace(punc, "")
-            cleanwords.append(x.lower())
-    return (cleanwords, cleansample)
+            if [pog.is_stop for pog in nlp(x)] == [False]:
+                cleanwords.append(x.lower())
+    return cleansample
 
+def attrSep(proc: spacy.tokens.doc.Doc) -> list:
+    info = proc.similarity(nlp('information facts assertion'))
+    pers = proc.similarity(nlp('imperative persuasive argument'))
+    desc = proc.similarity(nlp('descriptive demonstration workshop'))
+    entr = proc.similarity(nlp('entertaining witty engaging'))
+    cerm = proc.similarity(nlp('occasion ceremony tribute'))
+    return [info, pers, desc, entr, cerm]
+
+def analyse(data: list):
+    reslist = []
+    matchData = pd.read_csv('defdata.csv')
+    maxd = matchData.max().tolist()
+    mind = matchData.min().tolist()
+    for i in data:
+        cleansample = clean(i)
+        rawspeech = " ".join(cleansample.tolist())
+        proc = nlp(rawspeech)
+        attrList = attrSep(proc)
+        scaledattr = [((attrList[x] - mind[x])/(maxd[x] - mind[x])) for x in range(5)]
+        reslist.append(scaledattr)
+    return reslist
